1ea3ed3451611ca3875bb7a4cf0f70645fa8ae28
elie ntumba <elientumba@live.fr>
2018-03-14 19:21:44 +0800

    adding plugin to the model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,16 @@
 var mongoose = require('mongoose');
+var pasportLocalMongoose = require('passport-local-mongoose');
+
+
+var UserSchema = new mongoose.Schema(
+    {
+        username : String,
+        password : String
+    }
+);
+
+
+//step 2 adding mongoose plugin to the user model
+UserSchema.plugin(pasportLocalMongoose)
+
+module.exports = mongoose.model('User' , UserSchema);
